635b6cdb87470537f60e9e3356237766a6780935
Brendan Gasparin <brendan.gasparin@gmail.com>
2021-10-19 20:41:13 +1100

    Remove code

--- scripts.js
+++ scripts.js
@@ -12,21 +12,8 @@ function getVideo() {
             video.play();
         })
         .catch(err => {
-            console.error('Web cam denied!', err);
-        });
-}
-
-function paintToCanvas() {
-    const width = video.videoWidth;
-    const height = video.videoHeight;
-    canvas.width = width;
-    canvas.height = height;
-
-    setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height);
-    }, 16);
+            console.error('Web camera access denied.', err);
+        })
 }
 
 getVideo();
-
-video.addEventListener('canplay', paintToCanvas);
